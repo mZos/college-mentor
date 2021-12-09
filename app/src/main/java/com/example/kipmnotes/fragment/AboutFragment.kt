17c334de8a28ee156d5ac01baa3d8f46f9f66e8f
Zakariya <mohdzakariya272@gmail.com>
2021-12-09 14:15:54 +0530

    updated about page design

--- app/src/main/java/com/example/kipmnotes/fragment/AboutFragment.kt
+++ app/src/main/java/com/example/kipmnotes/fragment/AboutFragment.kt
@@ -12,51 +12,39 @@ import androidx.fragment.app.Fragment
 import com.example.kipmnotes.R
 import com.example.kipmnotes.databinding.FragmentAboutBinding
 
-class AboutFragment : Fragment() {
+class AboutFragment : Fragment(R.layout.fragment_about) {
 
     private lateinit var binding: FragmentAboutBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentAboutBinding.inflate(inflater, container, false)
-
-        // Linked with linkedIn Profile
-//        binding.txtAboutZakariya?.movementMethod = LinkMovementMethod.getInstance()
-        binding.txtAboutZakariya?.setOnClickListener {
-            Toast.makeText(activity as Context, "hello", Toast.LENGTH_SHORT).show()
-        }
-        binding.txtAboutMasoom?.movementMethod = LinkMovementMethod.getInstance()
-        binding.txtAboutVishal?.movementMethod = LinkMovementMethod.getInstance()
-
-        if (activity != null) {
-            binding.txtAboutZakariya?.setLinkTextColor(
-                ContextCompat.getColor(
-                    activity as Context,
-                    R.color.linkColor
-                )
-            )
-
-            binding.txtAboutMasoom?.setLinkTextColor(
-                ContextCompat.getColor(
-                    activity as Context,
-                    R.color.linkColor
-                )
-            )
-
-            binding.txtAboutVishal?.setLinkTextColor(
-                ContextCompat.getColor(
-                    activity as Context,
-                    R.color.linkColor
-                )
-            )
-        }
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.txtAboutZakariya?.text = "hello"
+        binding = FragmentAboutBinding.bind(view)
+
+        binding.txtAboutZakariya.movementMethod = LinkMovementMethod.getInstance()
+        binding.txtAboutMasoom.movementMethod = LinkMovementMethod.getInstance()
+        binding.txtAboutVishal.movementMethod = LinkMovementMethod.getInstance()
+
+        if (activity != null) {
+            binding.txtAboutZakariya.setLinkTextColor(
+                ContextCompat.getColor(
+                    activity as Context,
+                    R.color.linkColor
+                )
+            )
+
+            binding.txtAboutMasoom.setLinkTextColor(
+                ContextCompat.getColor(
+                    activity as Context,
+                    R.color.linkColor
+                )
+            )
+
+            binding.txtAboutVishal.setLinkTextColor(
+                ContextCompat.getColor(
+                    activity as Context,
+                    R.color.linkColor
+                )
+            )
+        }
     }
 }
